1108b8f97e29c39480ea7d43abed3400d5e7096c
Emre Sahin <mehmetemre9906@gmail.com>
2023-03-06 14:44:52 +0300

    updated

---
+++ Interactive Story/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Interactive Story
+//
+//  Created by Emre Sahin on 6.03.2023.
+//
+
+import Foundation
---
+++ Interactive Story/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Interactive Story
+//
+//  Created by Emre Sahin on 6.03.2023.
+//
+
+import Foundation
